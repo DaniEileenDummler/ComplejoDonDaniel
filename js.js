b955e5aa32b621440dbabd692730773476c2a968
DaniEileen <danieladummler@hotmail.com>
2023-11-08 19:33:41 -0300

    renombrando archivos

--- js.js
+++ js.js
@@ -1,13 +1,12 @@
-const url = 'https://script.google.com/macros/s/AKfycbwlLgbcbVpK8-LU4xM0AEmGWQAkmXWYKXeMSBe-L6wDaRVVZJFcZxtsyY_Wu8WzO6NN/exec';
-const form = document.forms['form-contact'];
+const url = 'https://script.google.com/macros/s/AKfycbzMov9nHGjQAX2ObdPJF1Tw9EEYQ3CJlMNLYMAEr24MUp3MS6d2c6fxKu2qWcqg_xYF/exec';
+const form = document.forms['contact'];
 
 form.addEventListener('submit', e => {
-  e.preventDefault();
+  e.preventDefault()
   fetch(url, { method: 'POST', body: new FormData(form)})
-    .then(response => alert('Enviado!', response))
-    .catch(error => console.log('Error!', error.message));
-});
-
+    .then(response =>alert('Enviado!', response))
+    .catch(error => console.error('Error!', error.message))
+})
 
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
