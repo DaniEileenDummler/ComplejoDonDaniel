3615b29b327c7cb1d09001613dc0eef8f2046372
DaniEileen <danieladummler@hotmail.com>
2023-11-08 17:23:19 -0300

    parte 2

--- js.js
+++ js.js
@@ -1,8 +1,18 @@
-const url = 'https://script.google.com/macros/s/AKfycbyAyv3tzeY1oNjzQs9PTXnJiBppxJ3EarsMWEIjdaQt52uR8VK8zP0mJ2vrVE4uyoU8/exec';
-const form = document.forms['contact'];
+const url = 'https://script.google.com/macros/s/AKfycbwlLgbcbVpK8-LU4xM0AEmGWQAkmXWYKXeMSBe-L6wDaRVVZJFcZxtsyY_Wu8WzO6NN/exec';
+const form = document.forms['form-contact'];
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   fetch(url, { method: 'POST', body: new FormData(form)})
     .then(response => alert('Enviado!', response))
     .catch(error => console.log('Error!', error.message));
+});
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  menuToggle.classList.toggle('active');
 });
